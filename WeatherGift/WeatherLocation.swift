c51396e6ec5f07da63dc76585b1d18fb63b7f389
Daniel Yang <yangafc@bc.edu>
2019-10-19 17:23:34 -0400

    completed 7.10 and 7.11

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Daniel Yang on 10/19/19.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
